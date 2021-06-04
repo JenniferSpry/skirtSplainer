8c970e647340930a98f2ac690721024ab2b3b4e1
JenniferSpry <jennifer.spry@mindmatters.de>
2021-06-04 20:09:10 +0200

    clean up and start playing with csv

---
+++ src/components/HomePage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <p>{{ 'hello' }}</p>
+    <skirt-circles :ax="parseInt(ax)"></skirt-circles>
+    <label>Ax</label>
+    <input type="number" v-model="ax">
+  </div>
+</template>
+
+<script>
+import SkirtCircles from './SkirtCircles.vue';
+
+export default {
+  name: 'HomePage',
+  components: { SkirtCircles },
+  data() {
+    return {
+      ax: 100,
+    };
+  },
+};
+</script>
