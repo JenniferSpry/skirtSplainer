83ed1f95520de560dc8f65f5968ad1ddcf3fd85e
JenniferSpry <jennifer.spry@mindmatters.de>
2021-06-16 17:07:53 +0200

    add colors and explanation

--- src/components/EllipsePlayground.vue
+++ src/components/EllipsePlayground.vue
@@ -1,14 +1,34 @@
 <template>
   <div>
     <v-row justify="center">
-      <svg xmlns="http://www.w3.org/2000/svg" :width="600" :height="320">
-        <path :d="pathArc" stroke="black" fill="none" stroke-width="2" fill-opacity="0.5"/>
-        <path :d="pathEllipse1" stroke="#6dcdd6" fill="none"/>
-        <path :d="pathEllipse2" stroke="#6dcdd6" fill="none"/>
-        <path :d="pathEllipse3" stroke="#6dcdd6" fill="none"/>
-        <circle :cx="startArcX" :cy="startArcY" r="4" fill="red"/>
-        <circle :cx="endArcX" :cy="endArcY" r="4" fill="blue"/>
-      </svg>
+      <v-card>
+        <svg xmlns="http://www.w3.org/2000/svg" :width="600" :height="320">
+          <path :d="pathArc" stroke="rgba(0,0,0,.6)" fill="none" stroke-width="2"/>
+          <path :d="pathEllipse1" stroke="#6dcdd6" fill="none"/>
+          <path :d="pathEllipse2" stroke="#6dcdd6" fill="none"/>
+          <path :d="pathEllipse3" stroke="#6dcdd6" fill="none"/>
+          <circle :cx="startArcX" :cy="startArcY" r="4" fill="red"/>
+          <circle :cx="endArcX" :cy="endArcY" r="4" fill="blue"/>
+        </svg>
+      </v-card>
+    </v-row>
+    <v-row justify="center">
+      <v-card :class="'explanation'">
+        <v-card-text class="large-text">
+            <v-tooltip v-for="(part, index) in arcDescription" :key="index">
+              <template v-slot:activator="{ on, attrs }">
+                <p v-bind="attrs" v-on="on" >
+                  <span v-for="(item, index) in part.textParts"
+                        :key="index"
+                        v-bind:style="{ color: item.color }">
+                    {{ item.text }}&nbsp;
+                  </span>
+                </p>
+              </template>
+              <span>{{ part.description }}</span>
+            </v-tooltip>
+        </v-card-text>
+      </v-card>
     </v-row>
     <v-row>
       <v-col>
@@ -18,7 +38,7 @@
           thumb-label="always"
           :max="width"
           min="0"
-          color="red"
+          :color="startArcXColor"
         ></v-slider>
         <v-slider
           v-model="startArcY"
@@ -26,7 +46,7 @@
           thumb-label="always"
           :max="height"
           min="0"
-          color="red"
+          :color="startArcYColor"
         ></v-slider>
       </v-col>
       <v-col>
@@ -36,7 +56,7 @@
           thumb-label="always"
           :max="width"
           min="0"
-          color="blue"
+          :color="endArcXColor"
         ></v-slider>
         <v-slider
           v-model="endArcY"
@@ -44,7 +64,7 @@
           thumb-label="always"
           :max="height"
           min="0"
-          color="blue"
+          :color="endArcYColor"
         ></v-slider>
       </v-col>
     </v-row>
@@ -57,7 +77,7 @@
           thumb-label="always"
           :max="height"
           min="0"
-          color="blue"
+          :color="rxColor"
         ></v-slider>
         <v-slider
           v-model="ry"
@@ -65,7 +85,7 @@
           thumb-label="always"
           :max="height"
           min="0"
-          color="blue"
+          :color="ryColor"
         ></v-slider>
         <v-slider
           v-model="xAxisRotation"
@@ -73,7 +93,7 @@
           thumb-label="always"
           :max="180"
           min="0"
-          color="blue"
+          :color="xAxisRotationColor"
         ></v-slider>
       </v-col>
       <v-col>
@@ -82,12 +102,14 @@
             <v-switch
               v-model="largeArc"
               label="Large Arc?"
+              :color="largeArcColor"
             ></v-switch>
           </v-col>
           <v-col>
             <v-switch
               v-model="sweep"
               label="Which ellipse?"
+              :color="sweepColor"
             ></v-switch>
           </v-col>
         </v-row>
@@ -104,14 +126,23 @@ export default {
       width: 600,
       height: 320,
       startArcX: 260,
+      startArcXColor: '#fc3d03',
       startArcY: 180,
+      startArcYColor: '#fc9403',
       rx: 50,
+      rxColor: '#58cf19',
       ry: 60,
-      xAxisRotation: 0,
+      ryColor: '#96ed68',
+      xAxisRotation: 20,
+      xAxisRotationColor: '#609c40',
       largeArc: false,
+      largeArcColor: '#a330b0',
       sweep: true,
+      sweepColor: '#6c30b0',
       endArcX: 340,
+      endArcXColor: '#038cfc',
       endArcY: 140,
+      endArcYColor: '#031cfc',
     };
   },
   computed: {
@@ -122,6 +153,47 @@ export default {
            ${this.largeArc ? 1 : 0} ${this.sweep ? 1 : 0}
            ${this.endArcX} ${this.endArcY}
            L ${this.width - 20} 20`;
+    },
+    arcDescription() {
+      return [
+        {
+          description: 'Move to x y',
+          textParts: [
+            { text: 'M' },
+            { text: '20' },
+            { text: `${this.height - 20}` },
+          ],
+        },
+        {
+          description: 'Line to x y',
+          textParts: [
+            { text: 'L' },
+            { text: this.startArcX, color: this.startArcXColor },
+            { text: this.startArcY, color: this.startArcYColor },
+          ],
+        },
+        {
+          description: 'Arc radius-x radius-y x-axis-rotation large-arc-flag sweep-flag x y',
+          textParts: [
+            { text: 'A' },
+            { text: this.rx, color: this.rxColor },
+            { text: this.ry, color: this.ryColor },
+            { text: this.xAxisRotation, color: this.xAxisRotationColor },
+            { text: `${this.largeArc ? 1 : 0}`, color: this.largeArcColor },
+            { text: `${this.sweep ? 1 : 0}`, color: this.sweepColor },
+            { text: this.endArcX, color: this.endArcXColor },
+            { text: this.endArcY, color: this.endArcYColor },
+          ],
+        },
+        {
+          description: 'Line to x y',
+          textParts: [
+            { text: 'L' },
+            { text: this.width - 20 },
+            { text: '20' },
+          ],
+        },
+      ];
     },
     pathEllipse1() {
       return `M ${this.startArcX} ${this.startArcY}
@@ -142,8 +214,17 @@ export default {
 };
 </script>
 
-<style scoped>
-svg {
-  border: 1px solid grey;
+<style lang="scss" scoped>
+.explanation {
+  min-width: 600px;
+  margin: 20px 0 60px 0;
+
+  p:last-child {
+    margin-bottom: 0;
+  }
+}
+.large-text {
+  font-size: 2.5em;
+  line-height: 1em;
 }
 </style>
